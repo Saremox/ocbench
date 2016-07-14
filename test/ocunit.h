d940e22b90a6e7c2b54a52a12b477fd1514e91f8
Saremox <saremox@linux.com>
2016-07-14 15:32:36 +0000

    Change format of unit test macro output
    Change the way the output of the unit testing macros look like
    also added an strcmp assert.

--- test/ocunit.h
+++ test/ocunit.h
@@ -5,29 +5,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define oc_assert(test, msg) if(!(test)) { log_err(msg); return msg; }
+
+#define oc_unit_output(msg, ...) {\
+  printf("\n│  │  └─ Line: %d " msg,__LINE__,##__VA_ARGS__);}
+#define oc_assert(test, msg) if(!(test)) {\
+  oc_unit_output(msg);\
+  return -1;}
 #define oc_assert_equal(expected, got) if(expected != got) {\
-  log_err("expected: <0x%x> but got : <0x%x>",expected,got)}
+  oc_unit_output("expected: <0x%x> but got : <0x%x>",expected,got)\
+  return -1;}
+#define oc_assert_str_equal(expected, got) if(strcmp(expected,got) != 0) {\
+  oc_unit_output("expected \"%s\" but got \"%s\"",expected,got );\
+  return -1;}
 #define oc_run_test(test) printf("\n│  ├─ Case #%04d %s:",tests_run, " " #test); \
-  lastmsg = test();\
+  lastret = test();\
   tests_run++;\
-  if(lastmsg != 0)\
-    return lastmsg;\
+  if(lastret != 0)\
+    return -1;\
   printf(" PASSED");\
   passed_test++;\
 
 #define RUN_TESTS(name) int main(int argc, char *argv[]) {\
     argc = 1; \
     printf("├─ RUNNING: %s", argv[0]);\
-        char *result = name();\
-        if (result != 0) {\
-            printf(" FAILED: %s\n", result);\
-        }\
+        name();\
     printf("\n│  └─ Tests passed : %d out of %d\n", passed_test, tests_run);\
         exit(0);\
 }
 
 int passed_test;
 int tests_run;
-char *lastmsg;
+int lastret;
 #endif
