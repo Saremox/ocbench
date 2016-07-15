b219f7fac58235e8df799d38d327f7cdab9ca1aa
Saremox <saremox@linux.com>
2016-07-15 20:21:23 +0000

    Add tests for ocsched
    testing process creation, child -> master and master -> child
    communication

---
+++ test/Testocsched.c
@@ -0,0 +1,97 @@
+#include "ocunit.h"
+#include "ocsched/ocsched.h"
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/ioctl.h>
+
+#define EXIT_TEST_OK 42
+#define TESTSTRING "Hello world!"
+
+int TestProcessCreationChild(ocschedProcessContext* ctx, void* data) {
+  oc_assert(ctx->pid==0, "pid should be zero in child");
+  exit(EXIT_TEST_OK);
+}
+
+int TestProcessCreation()
+{
+  int ret;
+  ocschedProcessContext * ctx =
+    ocsched_fork_process((ocschedFunction) TestProcessCreationChild,"test",NULL);
+
+  oc_assert(ctx->pid>0, "pid of child should be greater than 0");
+
+  waitpid(ctx->pid,&ret,0);
+  oc_assert_equal(EXIT_TEST_OK, WEXITSTATUS(ret));
+
+  return EXIT_SUCCESS;
+}
+
+int TestCommMasterToChildChild(ocschedProcessContext* ctx, void* data) {
+  int count = 0, tries = 0;
+  while (count < strlen(TESTSTRING) - 1 && tries <= 100) {
+    ioctl(ctx->comm.pipe_in, FIONREAD, &count);
+    usleep(100);
+    tries++;
+  }
+  oc_assert(tries < 100, " child timeout on reading pipe");
+  char buf[count+1];
+  read(ctx->comm.pipe_in,&buf,count);
+  buf[count] = 0x00;
+  oc_assert_str_equal(TESTSTRING,buf)
+
+  exit(EXIT_TEST_OK);
+}
+
+int TestCommMasterToChild()
+{
+  int ret;
+  ocschedProcessContext * ctx =
+    ocsched_fork_process((ocschedFunction) TestCommMasterToChildChild,"test",NULL);
+
+  write(ctx->comm.pipe_out,TESTSTRING,strlen(TESTSTRING));
+
+  waitpid(ctx->pid,&ret,0);
+  oc_assert_equal(EXIT_TEST_OK, WEXITSTATUS(ret));
+
+  return EXIT_SUCCESS;
+}
+
+int TestCommChildToMasterChild(ocschedProcessContext* ctx, void* data) {
+  write(ctx->comm.pipe_out,TESTSTRING,strlen(TESTSTRING));
+
+  exit(EXIT_TEST_OK);
+}
+
+int TestCommChildToMaster()
+{
+  int ret, count = 0, tries = 0;
+  ocschedProcessContext * ctx =
+    ocsched_fork_process((ocschedFunction) TestCommChildToMasterChild,"test",NULL);
+
+  while (count < strlen(TESTSTRING) - 1 && tries <= 100) {
+    ioctl(ctx->comm.pipe_in, FIONREAD, &count);
+    usleep(100);
+    tries++;
+  }
+  oc_assert(tries < 100, " child timeout on reading pipe");
+  char buf[count+1];
+  read(ctx->comm.pipe_in,&buf,count);
+  buf[count] = 0x00;
+  oc_assert_str_equal(TESTSTRING,buf)
+
+  waitpid(ctx->pid,&ret,0);
+  oc_assert_equal(EXIT_TEST_OK, WEXITSTATUS(ret));
+
+  return EXIT_SUCCESS;
+}
+
+int RunAllTests()
+{
+  oc_run_test(TestProcessCreation);
+  oc_run_test(TestCommMasterToChild);
+  oc_run_test(TestCommChildToMaster);
+
+  return EXIT_SUCCESS;
+}
+
+RUN_TESTS(RunAllTests);
--- test/runtests.sh
+++ test/runtests.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/sh
 ./Testocunit
 ./Testocmemfd
+./Testocsched
