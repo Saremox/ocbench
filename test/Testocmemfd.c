2cb2c8bc00bc7e1bbd96c25e48025bcdc6a3d3d6
Saremox <saremox@linux.com>
2016-07-19 14:06:35 +0000

    Add test cases for ocmemfd functions
    this adds test cases for context destroy and load file calls
    also add memory test with memset in all resize tests.
    also add new resize memfd tests

--- test/Testocmemfd.c
+++ test/Testocmemfd.c
@@ -1,5 +1,7 @@
 #include "ocmemfd/ocmemfd.h"
 #include "ocunit.h"
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #define SIZEOFMEMFD 512
 
@@ -31,11 +33,23 @@ int TestGetSeals()
 int TestResizingMemfdGrow()
 {
   int newsize = 1024;
-  int oldsize = SIZEOFMEMFD;
 
-  oc_assert_equal(oldsize, fd->size);
   oc_assert(ocmemfd_resize(fd,newsize) == OCMEMFD_SUCCESS,"resizing was not successful");
   oc_assert_equal(newsize, fd->size);
+  // Test if we can set every byte in memfd
+  memset(fd->buf,0x00,fd->size);
+
+  return EXIT_SUCCESS;
+}
+
+int TestResizingMemfdGrowBig()
+{
+  int newsize = 16 * 1024 * 1024;
+
+  oc_assert(ocmemfd_resize(fd,newsize) == OCMEMFD_SUCCESS,"resizing was not successful");
+  oc_assert_equal(newsize, fd->size);
+  // Test if we can set every byte in memfd
+  memset(fd->buf,0x00,fd->size);
 
   return EXIT_SUCCESS;
 }
@@ -43,22 +57,82 @@ int TestResizingMemfdGrow()
 int TestResizingMemfdShrink()
 {
   int newsize = SIZEOFMEMFD;
-  int oldsize = 1024;
 
-  oc_assert_equal(oldsize, fd->size);
   oc_assert(ocmemfd_resize(fd,newsize) == OCMEMFD_SUCCESS,"resizing was not successful");
   oc_assert_equal(newsize, fd->size);
+  // Test if we can set every byte in memfd
+  memset(fd->buf,0x00,fd->size);
+
+  return EXIT_SUCCESS;
+}
+
+int TestResizingMemfdShrinkTiny()
+{
+  int newsize = 16;
+
+  oc_assert(ocmemfd_resize(fd,newsize) == OCMEMFD_SUCCESS,"resizing was not successful");
+  oc_assert_equal(newsize, fd->size);
+  // Test if we can set every byte in memfd
+  memset(fd->buf,0x00,fd->size);
 
   return EXIT_SUCCESS;
 }
 
 int TestAfterResizeDataStillExists()
 {
-  int newsize = 768;
-  int oldsize = SIZEOFMEMFD;
+  int newsize = SIZEOFMEMFD + 256;
 
+  sprintf(fd->buf , "Test");
   ocmemfd_resize(fd,newsize);
   oc_assert_str_equal("Test", fd->buf);
+
+  return EXIT_SUCCESS;
+}
+
+int TestLoadFileIntoMemfd()
+{
+  int file;
+  struct stat st;
+  off_t filesize = 0;
+  char * path = "./Testocmemfd";
+  char * memorymap;
+
+  oc_unit_output("test %s\n",path);
+  if(stat(path,&st) ==0)
+    filesize = st.st_size;
+  else
+    log_err("cannot stat %s for testing",path);
+
+  oc_unit_output("Open %s in memfd\n",path);
+  oc_assert(ocmemfd_load_file(fd,path) == OCMEMFD_SUCCESS, "failed to load file");
+  oc_unit_output("Open %s and memory map it",path);
+  file = open(path,0,'r');
+  memorymap = mmap(NULL, filesize, PROT_READ,MAP_PRIVATE, file,0);
+
+  oc_assert(memorymap != MAP_FAILED, "failed to memorymap test file");
+  oc_unit_output("Beginning to compare memory of size %d",filesize);
+  oc_unit_output("fd->buf points to 0x%x",fd->buf);
+  oc_unit_output("memorymap points to 0x%x",memorymap);
+  sleep(2);
+  for (size_t i = 0; i < filesize; i++) {
+    oc_unit_output("test memorymap[%d] and fd->buf[%d] are equal\n",i,i);
+    oc_unit_output("memorymap[%d] = 0x%x\n",i,memorymap[i]);
+    oc_unit_output("fd->buf[%d] = 0x%x\n",i,fd->buf[i]);
+    oc_assert_equal(memorymap[i],fd->buf[i]);
+  }
+  oc_unit_output("Done memory test\n");
+  oc_assert_mem_equal(memorymap,fd->buf,filesize-1);
+
+  munmap(memorymap,filesize);
+  close(file);\
+
+  return EXIT_SUCCESS;
+}
+
+int TestDetroyContext()
+{
+  oc_assert(ocmemfd_destroy_context(&fd) == OCMEMFD_SUCCESS,
+    "failed to destroy context");
 
   return EXIT_SUCCESS;
 }
@@ -69,8 +143,12 @@ int RunAllTests()
   oc_run_test(TestGetSeals);
   oc_run_test(TestMemoryMap);
   oc_run_test(TestResizingMemfdGrow);
+  oc_run_test(TestResizingMemfdGrowBig);
   oc_run_test(TestResizingMemfdShrink);
+  oc_run_test(TestResizingMemfdShrinkTiny);
   oc_run_test(TestAfterResizeDataStillExists);
+  oc_run_test(TestLoadFileIntoMemfd);
+  oc_run_test(TestDetroyContext);
 
 
   return EXIT_SUCCESS;
