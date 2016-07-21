18c2ab34fcf3d84d092f6aa44f0d4b5d7fd1be10
Saremox <saremox@linux.com>
2016-07-21 15:16:41 +0000

    Add documentation to ocmemfd, ocsched

--- src/ocmemfd/ocmemfd.h
+++ src/ocmemfd/ocmemfd.h
@@ -32,13 +32,13 @@ typedef enum {
 }
 ocMemfdStatus;
 
-/** Creates a new context and manages systemcall for new memfd pointer
+/** Creates a new context and manages systemcall for new memfd/shm/tmpfile pointer
   * and memory maps it into virtual address space. See ocMemfdContext
   */
 ocMemfdContext * /**< [out] valid context for usage in ocmemfd functions */
 ocmemfd_create_context(
   char* path, /**< [in] the path in the kernel memfd file system */
-  size_t size /**< [in] the desired size of the memfd*/
+  size_t size /**< [in] the desired size of the memfd/shm/tmpfile*/
 );
 
 /** Resizes the given memfd in memfdcontext to newsize
@@ -47,21 +47,31 @@ ocmemfd_create_context(
 ocMemfdStatus /**< [out] on success OCMEMFD_SUCCESS */
 ocmemfd_resize(
   ocMemfdContext * ctx, /**< [in,out] the context */
-  size_t newsize /**< [in] the new desired size of the memfd */
+  size_t newsize /**< [in] the new desired size of the memfd/shm/tmpfile */
 );
 
+/** remap the context to ctx->size from oldsize.
+  * This get's invoked automaticly on resize
+  * must be called if memfd/shm/tmpfile get resized from other process
+  */
 ocMemfdStatus
 ocmemfd_remap_buffer(
   ocMemfdContext * ctx, /**< [in,out] the context */
-  size_t oldsize /**< [in] the old size of the memfd */
+  size_t oldsize /**< [in] the old size of the memfd/shm/tmpfile */
 );
 
+/** load the file given this path into the memfd/shm/tmpfile of
+  * the given context and resizes it if needed
+  */
 ocMemfdStatus /**< [out] on success OCMEMFD_SUCCESS */
 ocmemfd_load_file(
   ocMemfdContext * ctx, /**< [in,out] the context */
-  char* path /**< [in] path of the file that gets loaded into the memfd */
+  char* path /**< [in] path of the file that gets loaded into the memfd/shm/tmpfile */
 );
 
+/** destroying context by closing any open file descriptors and deallocates
+  * previously allocated memory
+  */
 ocMemfdStatus
 ocmemfd_destroy_context(
   ocMemfdContext ** ctx /**< [in,out] the context */
--- src/ocsched/ocsched.h
+++ src/ocsched/ocsched.h
@@ -29,6 +29,9 @@ typedef enum {
   OCSCHED_FAILURE = -1
 } ocschedStatus;
 
+/** forks the process, asign childname as new processname
+  * and calls work_function for child
+  */
 ocschedProcessContext *
 ocsched_fork_process(
   ocschedFunction work_function, /**< [in] function thats called by the child */
@@ -36,6 +39,8 @@ ocsched_fork_process(
   void* data /**< [in] pointer with addional data for the child */
 );
 
+/** send n bytes of buf to the given valid context
+  */
 size_t /**< [out] bytes written or OCSCHED_FAILURE on failure */
 ocsched_sendto(
   ocschedProcessContext * ctx, /**< [in] context which will be send to */
@@ -43,6 +48,9 @@ ocsched_sendto(
   size_t n /**< [in] number of bytes which will be send */
 );
 
+/** printf wrapper for usage with ocschedProcessContext
+  * fmtstr behaved like fmtstr in printf
+  */
 ocschedStatus
 ocsched_printf(
   ocschedProcessContext * ctx, /**< [in] context which will be send to */
@@ -50,6 +58,9 @@ ocsched_printf(
   ...
 );
 
+/** revc n bytes into buf from the given valid context
+  *
+  */
 size_t /**< [out] bytes readed or OCSCHED_FAILURE on failure */
 ocsched_recvfrom(
   ocschedProcessContext * ctx, /**< [in] context which will be recv from */
@@ -57,21 +68,33 @@ ocsched_recvfrom(
   size_t n /**< [in] maximum byte count which will be recv */
 );
 
+/** STUB
+  *
+  */
 ocschedStatus
 ocsched_schedule_job(
   ocschedJob * job /**< [in] job which get scheduled */
 );
 
+/** STUB
+  *
+  */
 ocschedStatus
 ocsched_get_job(
   ocschedJob * job /**< [out] pointer in which we store the job */
 );
 
+/** clean up context. closes open file descriptors and free's allocated memory
+  * only the parent need to call this function. children get automatically
+  * cleaned up
+  */
 ocschedStatus
 ocsched_destroy_context(
   ocschedProcessContext ** ctx /**< [in] context which will be recv from */
 );
 
+/** unlinks the global ipc message queue
+  */
 ocschedStatus
 ocsched_destroy_global_mqueue();
 #endif
