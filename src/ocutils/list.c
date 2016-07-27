92d3c3caedaf3a5197f7076c185eb2b395cb1383
Saremox <saremox@linux.com>
2016-07-27 13:48:36 +0000

    Add ocutils list implementation
    simple double linked implementation. provides functions to use
    the list as a stack (push/pop) and as a queue (enqueu/dequeue)

---
+++ src/ocutils/list.c
@@ -0,0 +1,135 @@
+#include <stdlib.h>
+#include "debug.h"
+#include "ocutils/list.h"
+
+List* ocutils_list_create()
+{
+  return calloc(1,sizeof(List));
+}
+
+void  ocutils_list_destroy(List* list)
+{
+  ocutils_list_foreach_f(list, cur)
+  {
+    if(cur->prev)
+      free(cur->prev);
+  }
+  if(ocutils_list_tail(list))
+    free(ocutils_list_tail(list));
+  free(list);
+}
+
+void  ocutils_list_remptr (List* list, void* val)
+{
+  int64_t position = ocutils_list_getpos(list,val);
+  check(position > 0,"cannot find the given value in the list");
+  ocutils_list_rempos(list,position);
+error:
+  return;
+}
+
+void  ocutils_list_addpos (List* list, void* val, int64_t position)
+{
+  check(position <= ocutils_list_size(list),
+    "invalid position. greater than size of list");
+  ListNode* newnode = calloc(1,sizeof(ListNode));
+  newnode->value = val;
+  if(position == ocutils_list_size(list))
+  {
+    newnode->prev = list->tail;
+    list->tail = list->tail->next = newnode;
+  }
+  else if(position == 0)
+  {
+    newnode->next =list->head;
+    list->head = list->head->prev = newnode;
+  }
+  else
+  {
+    ListNode* prev = NULL;
+    ListNode* next = list->head;
+    for(int64_t curPos = 0; curPos < position; curPos++)
+    {
+      prev = next;
+      next = next->next;
+    }
+    prev->next = newnode;
+    next->prev = newnode;
+    newnode->prev = prev;
+    newnode->next = next;
+  }
+error:
+  return;
+}
+
+void* ocutils_list_rempos (List* list, int64_t position)
+{
+  check(position <= ocutils_list_size(list),
+    "invalid position. greater than size of list");
+  void* val;
+  if(position == list->items)
+  {
+    val = list->tail->value;
+    list->tail = list->tail->prev;
+    free(list->tail->next);
+    list->tail->next = NULL;
+  }
+  else if(position == 0)
+  {
+    val = list->head->value;
+    list->head = list->head->next;
+    free(list->head->prev);
+    list->head->prev = NULL;
+  }
+  else
+  {
+    ListNode* prev = NULL;
+    ListNode* next = list->head;
+    for(int64_t curPos = 0; curPos < position; curPos++)
+    {
+      prev = next;
+      next = next->next;
+    }
+    val = next->value;
+    prev->next = next->next;
+    next->next->prev = prev;
+    free(next);
+  }
+
+  return val;
+error:
+  return (void*) -1;
+}
+
+int64_t ocutils_list_getpos (List* list, void* value)
+{
+  int64_t curPos = 0;
+  ListNode* prev = NULL;
+  ListNode* next = list->head;
+  for(; curPos < list->items; curPos++)
+  {
+    if(next->value == value)
+      break;
+    prev = next;
+    next = next->next;
+  }
+  if(next->value == value)
+    return curPos;
+  else
+    return -1;
+}
+
+void* ocutils_list_get (List* list, int64_t position)
+{
+  void* val;
+  ListNode* prev = NULL;
+  ListNode* next = list->head;
+  for(int64_t curPos = 0; curPos < position; curPos++)
+  {
+    prev = next;
+    next = next->next;
+  }
+  val = next->value;
+
+  return val;
+}
---
+++ src/ocutils/list.h
@@ -0,0 +1,63 @@
+#include <stdint.h>
+
+#ifndef LIST_H
+#define LIST_H
+
+struct __List;
+struct __ListNode;
+
+typedef struct __List     List;
+typedef struct __ListNode ListNode;
+
+struct __List {
+  int64_t   items;
+  ListNode* head;
+  ListNode* tail;
+};
+
+struct __ListNode {
+  ListNode* next;
+  ListNode* prev;
+  void*     value;
+};
+
+#define ocutils_list_size(LIST) LIST->items
+#define ocutils_list_head(LIST) LIST->head
+#define ocutils_list_tail(LIST) LIST->tail
+
+List*   ocutils_list_create ();
+void    ocutils_list_destroy(List* list);
+void    ocutils_list_remptr (List* list, void* val);
+void    ocutils_list_addpos (List* list, void* val, int64_t position);
+void*   ocutils_list_rempos (List* list, int64_t position);
+void*   ocutils_list_get    (List* list, int64_t position);
+int64_t ocutils_list_getpos (List* list, void* val);
+
+#define ocutils_list_add(LIST, VALUE) \
+  ocutils_list_addpos(LIST, VALUE, ocutils_list_size(LIST))
+
+#define ocutils_list_push(LIST, VALUE) \
+  ocutils_list_addpos(LIST, VALUE, 0)
+
+#define ocutils_list_pop(LIST) \
+  ocutils_list_rempos(LIST,0)
+
+#define ocutils_list_enqueue(LIST, VALUE) \
+  ocutils_list_addpos(LIST, VALUE, ocutils_list_size(LIST))
+
+#define ocutils_list_dequene(LIST) \
+  ocutils_list_rempos(LIST,0)
+
+#define ocutils_list_foreach_f(LIST, ITERATOR) ListNode* _node = NULL;\
+  ListNode* ITERATOR = NULL;\
+  for(ITERATOR = _node = LIST->head;\
+      _node != NULL;\
+      ITERATOR = _node = _node->next)
+
+#define ocutils_list_foreach_b(LIST, ITERATOR) ListNode* _node = NULL;\
+  ListNode* ITERATOR = NULL;\
+  for(ITERATOR = _node = LIST->tail;\
+      _node != NULL;\
+      ITERATOR = _node = _node->prev)
+
+#endif
