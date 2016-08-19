e0978008fdb4f91e4f53695f76ce395a30af7d02
Saremox <saremox@linux.com>
2016-08-19 16:18:49 +0000

    Add more tests for ocutils:list

--- test/Testocutils-list.c
+++ test/Testocutils-list.c
@@ -1,7 +1,7 @@
 #include "ocunit.h"
 #include "ocutils/list.h"
 
-int verbosityLevel = OCDEBUG_DEBUG;
+int verbosityLevel = OCDEBUG_ERROR;
 
 List* testlist;
 
@@ -57,8 +57,8 @@ int TestOcutilsListQueueEnqueueOrder()
   char* testStr1 = "I'm a test String";
   char* testStr2 = "I'm a test String too";
 
-  ocutils_list_add(testlist,testStr1);
-  ocutils_list_add(testlist,testStr2);
+  ocutils_list_enqueue(testlist,testStr1);
+  ocutils_list_enqueue(testlist,testStr2);
 
   oc_assert_equal_64bit(0, ocutils_list_getpos(testlist,testStr1));
   oc_assert_equal_64bit(1, ocutils_list_getpos(testlist,testStr2));
@@ -73,11 +73,28 @@ int TestOcutilsListQueueDequeueOrder()
   char* testStr1 = "I'm a test String";
   char* testStr2 = "I'm a test String too";
 
-  ocutils_list_add(testlist,testStr1);
-  ocutils_list_add(testlist,testStr2);
+  ocutils_list_enqueue(testlist,testStr1);
+  ocutils_list_enqueue(testlist,testStr2);
 
   oc_assert_str_equal(testStr1, ocutils_list_dequeue(testlist));
   oc_assert_str_equal(testStr2, ocutils_list_dequeue(testlist));
+
+  destroyTestlist();
+}
+
+int TestOcutilsListDequeueMoreThanEnqueued()
+{
+  initTestlist();
+
+  char* testStr1 = "I'm a test String";
+  char* testStr2 = "I'm a test String too";
+
+  ocutils_list_enqueue(testlist,testStr1);
+  ocutils_list_enqueue(testlist,testStr2);
+
+  oc_assert_str_equal(testStr1, ocutils_list_dequeue(testlist));
+  oc_assert_str_equal(testStr2, ocutils_list_dequeue(testlist));
+  oc_assert_equal_64bit(NULL, ocutils_list_dequeue(testlist));
 
   destroyTestlist();
 }
@@ -114,7 +131,22 @@ int TestOcutilsListStackPopOrder()
   destroyTestlist();
 }
 
-int TestOcutilsListStackPopMoreThanPushed
+int TestOcutilsListStackPopMoreThanPushed()
+{
+  initTestlist();
+
+  char* testStr1 = "I'm a test String";
+  char* testStr2 = "I'm a test String too";
+
+  ocutils_list_push(testlist,testStr1);
+  ocutils_list_push(testlist,testStr2);
+
+  oc_assert_str_equal(testStr2, ocutils_list_pop(testlist));
+  oc_assert_str_equal(testStr1, ocutils_list_pop(testlist));
+  oc_assert_equal_64bit(NULL, ocutils_list_pop(testlist));
+
+  destroyTestlist();
+}
 
 int RunAllTests()
 {
@@ -123,8 +155,10 @@ int RunAllTests()
   oc_run_test(TestOcutilsListAddOrder);
   oc_run_test(TestOcutilsListQueueEnqueueOrder);
   oc_run_test(TestOcutilsListQueueDequeueOrder);
+  oc_run_test(TestOcutilsListDequeueMoreThanEnqueued);
   oc_run_test(TestOcutilsListStackPushOrder);
   oc_run_test(TestOcutilsListStackPopOrder);
+  oc_run_test(TestOcutilsListStackPopMoreThanPushed);
 
   return EXIT_SUCCESS;
 }
